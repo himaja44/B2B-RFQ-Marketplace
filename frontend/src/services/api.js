d6426566b046d3609fa51df4abaf8aca70c1deee
Himaja <himajamailavarapu6@gmail.com>
2026-09-20 23:16:33 +0530

    Fix deployed API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://b2b-rfq-marketplace-o5oa.onrender.com/api"
+  baseURL: "https://b2b-rfq-marketplace-o5oa.onrender.com/api",
 });
 
 export default api;
