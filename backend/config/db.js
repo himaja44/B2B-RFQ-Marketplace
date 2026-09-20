2bb0ff0acefdc93af63296aafebc84cf86cde67f
Himaja <himajamailavarapu6@gmail.com>
2026-09-20 23:23:04 +0530

    Update database configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,32 +1,29 @@
 const mysql = require("mysql2");
-
 require("dotenv").config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-
+    port: process.env.DB_PORT || 4000,
     user: process.env.DB_USER,
-
     password: process.env.DB_PASSWORD,
-
     database: process.env.DB_NAME,
 
+    ssl: {
+        rejectUnauthorized: false
+    },
+
     waitForConnections: true,
-
     connectionLimit: 10,
-
     queueLimit: 0
 });
 
 pool.getConnection((err, connection) => {
-
     if (err) {
         console.error("Database connection failed:", err.message);
         return;
     }
 
-    console.log("MySQL Database Connected Successfully");
-
+    console.log("TiDB Cloud Database Connected Successfully");
     connection.release();
 });
 
